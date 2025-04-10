8175f35b9f84709a2fbe3ae7058ba0a5a0ff343d
zhamby1 <36927788+zhamby1@users.noreply.github.com>
2025-04-10 10:41:31 -0400

    d

--- App.js
+++ App.js
@@ -1,44 +1,9 @@
-import React, { useState } from "react";
-import { View, Text } from "react-native";
-import ConfirmationModal from "./ConfirmationModal";
-import ConfirmationAlert from "./ConfirmationAlert";
-import styles from "./styles";
+import React from "react";
+import {Text, View } from "react-native";
+import ToastComponent from "./ToastComponent";
 
-export default function App() {
-  const [modalVisible, setModalVisible] = useState(false);
-  const [alertVisible, setAlertVisible] = useState(false);
-
-  function toggleModal() {
-    setModalVisible(!modalVisible);
-  }
-
-  function toggleAlert() {
-    setAlertVisible(!alertVisible);
-  }
-
-  return (
-    <View style={styles.container}>
-      <ConfirmationModal
-        animationType="fade"
-        visible={modalVisible}
-        onPressConfirm={toggleModal}
-        onPressCancel={toggleModal}
-      />
-      <ConfirmationAlert
-        title="Are you sure?"
-        message="For realz?"
-        visible={alertVisible}
-        buttons={[
-          { text: "Nope", onPress: toggleAlert },
-          { text: "Yep", onPress: toggleAlert },
-        ]}
-      />
-      <Text style={styles.text} onPress={toggleModal}>
-        Show Confirmation Modal
-      </Text>
-      <Text style={styles.text} onPress={toggleAlert}>
-        Show Confimation Alert
-      </Text>
-    </View>
-  );
+export default App = () => {
+  return(
+    <ToastComponent />
+  )
 }
---
+++ ToastComponent.js
@@ -0,0 +1,8 @@
+import Toast from "react-native-toast-message";
+export default function ToastComponent() {
+    Toast.show({
+        type: "success",
+        text1: "Hello",
+        text2: "This is some something",
+    })
+}
