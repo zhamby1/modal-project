501d8239627857e9ccf8f4b0fdea2c4c5da64277
zhamby1 <36927788+zhamby1@users.noreply.github.com>
2025-04-10 11:07:59 -0400

    add

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Toast from 'react-native-toast-message'
 import {NavigationContainer} from '@react-navigation/native'
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
-import Home from './Home'
+import HomeScreen from './Home'
 
 
 const Stack = createNativeStackNavigator()
